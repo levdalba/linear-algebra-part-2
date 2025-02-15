3515ea56bff463275ecca9543d6f9158f41fcb5a
Levan Dalbashvili <levandalbashvili9@gmail.com>
2025-02-15 09:51:49 +0000

    96%

--- .laborantum/texts/Linear Algebra Part II/Determinants/Permutations Formula/task.py
+++ .laborantum/texts/Linear Algebra Part II/Determinants/Permutations Formula/task.py
@@ -1,39 +1,68 @@
-import json 
-import os 
-from pathlib import Path 
- 
-home_dir = Path(__file__).parent.resolve() 
- 
-answer = { 
-        "2x2": { 
-            "+": sorted([ 
-                '1 2']),  
-            "-": sorted(["2 1"]) 
-            }, 
-        "3x3": { 
-            "+": sorted([ 
-        "1 2 3","3 1 2","2 3 1" 
-            ]), 
-            "-": sorted([ 
-        "1 3 2", "2 1 3", "3 2 1" 
-            ]) 
-        }, 
-        "4x4": { 
-            "+": sorted([ 
-            "1 2 3 4", "1 3 4 2", "1 4 2 3", 
-            "2 1 4 3", "2 3 1 4", "2 4 3 1", 
-            "3 1 2 4", "3 2 4 1", "3 4 1 2", 
-            "4 1 3 2", "4 2 1 3", "4 3 2 1" 
-        ]), 
-            "-": sorted([ 
-            "1 2 4 3", "1 3 2 4", "1 4 3 2", 
-            "2 1 3 4", "2 3 4 1", "2 4 1 3", 
-            "3 1 4 2", "3 2 1 4", "3 4 2 1", 
-            "4 1 2 3", "4 2 3 1", "4 3 1 2" 
-        ]) 
-        } 
-    } 
- 
-json.dump( 
-    answer,  
+import json
+import os
+from pathlib import Path
+
+home_dir = Path(__file__).parent.resolve()
+
+answer = {
+        "2x2": {
+            "+": sorted([
+                ### YOUR CODE HERE ###
+                "1 2"
+            ]),
+            "-": sorted([
+                ### YOUR CODE HERE ###
+                "2 1"
+            ])
+        },
+        "3x3": {
+            "+": sorted([
+                ### YOUR CODE HERE ###
+                "1 2 3",
+                "2 3 1",
+                "3 1 2"
+            ]),
+            "-": sorted([
+                ### YOUR CODE HERE ###
+                "1 3 2",
+                "2 1 3",
+                "3 2 1"
+            ])
+        },
+        "4x4": {
+            "+": sorted([
+                ### YOUR CODE HERE ###
+                "1 2 3 4",
+                "1 3 4 2",
+                "1 4 2 3",
+                "2 1 4 3",
+                                "2 3 1 4",
+                "2 4 3 1",
+                "3 1 2 4",
+                "3 2 4 1",
+                                "3 4 1 2",
+                "4 1 3 2",
+                "4 2 1 3",
+                "4 3 2 1"
+            ]),
+            "-": sorted([
+                ### YOUR CODE HERE ###
+                "1 2 4 3",
+                "1 3 2 4",
+                "1 4 3 2",
+                "2 1 3 4",
+                                "2 3 4 1",
+                "2 4 1 3",
+                "3 1 4 2",
+                "3 2 1 4",
+                                "3 4 2 1",
+                "4 1 2 3",
+                "4 2 3 1",
+                "4 3 1 2"
+            ])
+        }
+    }
+
+json.dump(
+    answer, 
     open(home_dir/Path('.answer.json'), "w+"))
